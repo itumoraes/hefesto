97118f97684ac56d8903a2e4835001a15f303490
Italo Moraes <itu.moraes@gmail.com>
2016-10-14 11:37:55 -0300

    Add user gravatar url

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :cas_authenticatable, :trackable
+
+  def gravatar_url
+    require 'digest'
+
+    md5 = Digest::MD5.new
+    md5.update email
+    "https://www.gravatar.com/avatar/#{md5.hexdigest}?s=40"
+  end
 end
