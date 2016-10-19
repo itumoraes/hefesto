eade75ab2381100d7706f5e09dd7716ae4f42d2c
Italo Moraes <itu.moraes@gmail.com>
2016-10-19 14:39:10 -0200

    Improve create home office integration spec

--- spec/features/user/user_schedules_new_home_office_date_spec.rb
+++ spec/features/user/user_schedules_new_home_office_date_spec.rb
@@ -2,16 +2,23 @@
 require 'rails_helper'
 
 feature 'User schedules a new home office date', js: true do
+  let(:date) { Date.new(2016,10,19) }
+  before { Timecop.freeze(date) }
+
   scenario 'successfully' do
     user = create(:user)
     login_as user
 
     visit root_path
 
-    click_on 1.day.from_now.strftime('%Y-%m-%d')
+    find('[data-date="2016-10-19"]').click
     expect(page).to have_content 'Deseja marcar HO neste dia?'
     click_on 'Confirmar'
 
     expect(page).to have_content 'Home office marcado com sucesso!'
+    within '[data-date="2016-10-19"]' do
+      name = find('img')[:alt]
+      expect(name).to have_content(user.name)
+    end
   end
 end
