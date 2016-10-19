42decddec554ec3c626a945527c7b52a8732b9ee
Italo Moraes <itu.moraes@gmail.com>
2016-10-19 17:52:31 -0200

    Create integration spec to validate many home offices on the page

--- spec/features/user/user_schedules_new_home_office_date_spec.rb
+++ spec/features/user/user_schedules_new_home_office_date_spec.rb
@@ -2,17 +2,16 @@
 require 'rails_helper'
 
 feature 'User schedules a new home office date', js: true do
-  let(:date) { Date.new(2016,10,19) }
+  let(:date) { Date.new(2016, 10, 19) }
   before { Timecop.freeze(date) }
 
   scenario 'successfully' do
     user = create(:user)
-    login_as user
 
+    login_as user
     visit root_path
 
     find('[data-date="2016-10-19"]').click
-    expect(page).to have_content 'Deseja marcar HO neste dia?'
     click_on 'Confirmar'
 
     expect(page).to have_content 'Home office marcado com sucesso!'
