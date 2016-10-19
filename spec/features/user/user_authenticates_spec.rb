d567bec1550ee7aac7ec971fe6b3cc45d06a85aa
Débora Fernandes <debora.costa@locaweb.com.br>
2016-10-19 10:33:30 -0200

    Sign out user on CAS

--- spec/features/user/user_authenticates_spec.rb
+++ spec/features/user/user_authenticates_spec.rb
@@ -2,12 +2,24 @@
 require 'rails_helper'
 
 feature 'User authenticates' do
-  scenario 'successfully' do
+  scenario 'signs in' do
     user = create(:user)
     login_as(user)
 
     visit root_path
 
     expect(page).to have_content "#{user.name} (#{user.username})"
+  end
+
+  scenario 'signs out' do
+    user = create(:user)
+    login_as(user)
+    visit root_path
+
+    within('.ls-topbar') do
+      click_on 'Sair'
+    end
+
+    expect(current_path).to include('/')
   end
 end
